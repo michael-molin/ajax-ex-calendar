eeae5651a6af103a43d827ddf8831b4098665877
Michael <m.molin.1987@gmail.com>
2020-04-06 13:31:16 +0200

    bozza calendario funzionante

--- js/main.js
+++ js/main.js
@@ -1,50 +1,35 @@
 $(document).ready(function () {
-
     var source = $('#calendario-template').html();
     var template = Handlebars.compile(source);
 
 
-
-
     var dataSelezionata = moment("2018-01-01");
-    var meseSelezionato = dataSelezionata.format("MM");
-    console.log(meseSelezionato);
-
-
+    var dataLimiteIniziale = moment('2018-01-01');
+    var dataLimiteFinale = moment('2018-12');
     creaLista(dataSelezionata);
-    richiamaFeste(meseSelezionato);
+    richiamaFeste(dataSelezionata);
 
 
     $('.btn-succ').click(function() {
-        if (meseSelezionato == 12) {
-            dataSelezionata = moment("2018-01-01");
-            nomeMese = dataSelezionata.format("MMMM");
-            meseSelezionato = dataSelezionata.format("MM");
-        } else {
-            dataSelezionata.add(1, "month");
-            nomeMese = dataSelezionata.format("MMMM");
-            meseSelezionato = dataSelezionata.format("MM");
-        }
+        $('.btn-prev').prop('disabled', false);
+        dataSelezionata.add(1, "month");
+        creaLista(dataSelezionata);
+        richiamaFeste(dataSelezionata);
 
-        $('.mese').text(nomeMese);
-        creaLista(dataSelezionata, nomeMese);
-        richiamaFeste(meseSelezionato);
+        if (dataSelezionata.isSameOrAfter(dataLimiteFinale)) {
+            $('.btn-succ').prop('disabled', true);
+        }
     });
 
     $('.btn-prev').click(function() {
-        if (meseSelezionato == 01) {
-            dataSelezionata = moment("2018-12-01");
-            nomeMese = dataSelezionata.format("MMMM");
-            meseSelezionato = dataSelezionata.format("MM");
-        } else {
-            dataSelezionata.subtract(1, "month");
-            nomeMese = dataSelezionata.format("MMMM");
-            meseSelezionato = dataSelezionata.format("MM");
-        }
+        $('.btn-succ').prop('disabled', false);
+        dataSelezionata.subtract(1, "month");
+        creaLista(dataSelezionata);
+        richiamaFeste(dataSelezionata);
 
-        $('.mese').text(nomeMese);
-        creaLista(dataSelezionata, nomeMese);
-        richiamaFeste(meseSelezionato);
+        if (dataSelezionata.isSameOrBefore(dataLimiteIniziale)) {
+            $('.btn-prev').prop('disabled', true);
+        }
     });
 
 
@@ -55,6 +40,11 @@ $(document).ready(function () {
         var giorniTotaliMese = data.daysInMonth();
         $('.mese').text(nomeMese);
 
+        var x = 1;
+        while (x < data.isoWeekday()) {
+            $('.ul-giorni').append("<li class='giorno-vuoto'> </li>");
+            x++;
+        }
 
         for (var i = 1; i <= giorniTotaliMese; i++) {
             var questoGiorno = {
@@ -64,17 +54,18 @@ $(document).ready(function () {
 
             var giornoCompilato = template(questoGiorno);
             $('.ul-giorni').append(giornoCompilato);
+
             dataDaGestire.add(1 , "day");
         }
     }
 
-    function richiamaFeste(mese) {
+    function richiamaFeste(data) {
         $.ajax ({
             url: 'https://flynn.boolean.careers/exercises/api/holidays',
             method: 'GET',
             data: {
                 year: 2018,
-                month: (mese-1)
+                month: data.month()
             },
             success: function(data) {
                 var listaFestivi = data.response;
